0bdbce8325a3a473229ab5794ce14e6b1dcc710f
Matthew Victor <vicmcnabby@gmail.com>
2017-11-30 14:21:36 -0700

    added heroku api

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,10 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/chasefilms'
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL
   }
 
 };
