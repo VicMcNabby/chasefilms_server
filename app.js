4cf3fa0f68a254f7831f3e18a4d778595f000bf6
Matthew Victor <vicmcnabby@gmail.com>
2017-11-30 20:55:31 -0700

    server maintenance

--- app.js
+++ app.js
@@ -11,15 +11,7 @@ const jwt = require('jsonwebtoken');
 // var authMiddleware = require('./auth/middleware.js');
 const app = express();
 const router = require('./api/router');
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -28,8 +20,8 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser())
 
-
 app.use('/api/v1', router);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
